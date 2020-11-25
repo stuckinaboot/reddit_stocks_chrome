cf7da1c87c5780ae0f5f474b41e19e374e0b1949
Aspyn Palatnick <paluter@yahoo.com>
2020-11-24 23:12:42 -0500

    cleanup

--- RedditSearchHelper/content-script.js
+++ RedditSearchHelper/content-script.js
@@ -1,19 +1,14 @@
 function enrichSearchResults(contextNode = document) {
-  var googleSearchLinks = document.evaluate(
-    // "//div[@class=\"g\"]//a[starts-with(@href, 'https://www.reddit.com') and not(@rsh-processed)]",
+  const googleSearchLinks = document.evaluate(
     `//div[contains(@class,"Post")]//h3[not(@rsh-processed)]`,
     contextNode,
     null,
     XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
     null
   );
-  console.log("FISH", googleSearchLinks.snapshotLength);
 
   for (let i = 0; i < googleSearchLinks.snapshotLength; i++) {
-    // let link = googleSearchLinks.snapshotItem(i).getAttribute("href");
     googleSearchLinks.snapshotItem(i).setAttribute("rsh-processed", true);
-    // if (!link.includes("/comments/")) {
-    console.log("POTATO");
     const params = {
       targetStrike: 105,
       targetExpDate: 1606435200,
@@ -23,11 +18,9 @@ function enrichSearchResults(contextNode = document) {
     chrome.runtime.sendMessage(
       {
         contentScriptQuery: "fetchOption",
-        // "fetchRedditUrl",
-        // url: link,
         ...params,
       },
-      function (response) {
+      (response) => {
         if (response.error) {
           return;
         }
@@ -35,11 +28,7 @@ function enrichSearchResults(contextNode = document) {
         redditInfo.setAttribute("class", "rsh-wrapper");
 
         function addIconAndText(icon, text) {
-          // var icon = document.createElement("img");
-          // icon.setAttribute("class", "rsh-icon");
-          // icon.setAttribute("src", chrome.extension.getURL(iconPath));
-          // redditInfo.append(icon);
-          let textElement = document.createElement("a");
+          const textElement = document.createElement("a");
 
           // Open link in new window on click
           textElement.setAttribute(
@@ -52,7 +41,6 @@ function enrichSearchResults(contextNode = document) {
           redditInfo.append(textElement);
         }
 
-        // addIconAndText("assets/calendar_icon.png", response.date);
         addIconAndText("📈", params.ticker + ": $" + response.marketPrice);
         addIconAndText(
           "💵",
@@ -67,19 +55,14 @@ function enrichSearchResults(contextNode = document) {
           .insertAdjacentElement("afterend", redditInfo);
       }
     );
-    // }
   }
 }
 
 enrichSearchResults();
 
-// let additionalSearchResultsElement = document.getElementById("extrares");
 let observer = new MutationObserver(function (mutations) {
-  console.log("HIT THIS HERE");
-  mutations.forEach(function (mutation) {
+  mutations.forEach((mutation) => {
     const targetNodeName = mutation.target.nodeName;
-    // let targetId = mutation.target.getAttribute("id");
-    // if (targetId && targetId.startsWith("arc-srp")) {
     if (targetNodeName === "DIV" || targetNodeName === "div") {
       for (let addedNode of mutation.addedNodes) {
         enrichSearchResults(addedNode);
@@ -88,12 +71,8 @@ let observer = new MutationObserver(function (mutations) {
   });
 });
 
-observer.observe(
-  document,
-  // additionalSearchResultsElement
-  {
-    childList: true,
-    subtree: true,
-    attributes: false,
-  }
-);
+observer.observe(document, {
+  childList: true,
+  subtree: true,
+  attributes: false,
+});
